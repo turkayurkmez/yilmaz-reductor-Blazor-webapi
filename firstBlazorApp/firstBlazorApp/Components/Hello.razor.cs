752b769c6f9c290140c669d8a95961bed57628ab
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-05-29 12:22:48 +0300

    start with template

---
+++ firstBlazorApp/firstBlazorApp/Components/Hello.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace firstBlazorApp.Components
+{
+    public partial class Hello
+    {
+        [Parameter]
+        [EditorRequired]
+        public string Meeting { get; set; } = "İyi günler";
+    }
+}
